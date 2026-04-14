f470ca017e96a999e7d3b83a957077f6631d42ce
CalvinH008 <tololcapung@gmail.com>
2026-04-14 16:35:57 +0700

    feat(auth): create login controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,14 +7,16 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function showLogin(){
+    public function showLogin()
+    {
         return view('auth.login');
     }
 
-    public function login(Request $request){
+    public function login(Request $request)
+    {
         $credentials = $request->validate([
             'email' => ['required', 'email'],
-            'password' => ['required']
+            'password' => ['required', 'string']
         ]);
 
         if(Auth::attempt($credentials)){
@@ -22,7 +24,7 @@ class AuthController extends Controller
 
             return redirect()->intended('dashboard');
         }
-        
+
         return back()->withErrors([
             'email' => 'The provided credentials do not match our records.'
         ])->onlyInput('email');
