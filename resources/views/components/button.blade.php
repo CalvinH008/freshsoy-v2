f9770e43dc10f0513ecc78899a36abe42fd12aab
CalvinH008 <tololcapung@gmail.com>
2026-05-09 13:58:08 +0700

    refactor(components): add size prop to button component

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,4 +1,4 @@
-@props(['type' => 'submit', 'variant' => 'primary'])
+@props(['type' => 'submit', 'variant' => 'primary', 'size' => 'md'])
 @php
     $classes = match ($variant) {
         'primary' => 'bg-green-600 text-white hover:bg-green-700',
@@ -6,7 +6,14 @@
         'secondary' => 'bg-gray-200 text-gray-800 hover:bg-gray-300',
         default => 'bg-green-600 text-white hover:bg-green-700',
     };
+    $sizeClasses = match ($size) {
+        'sm' => 'px-3 py-1 text-xs',
+        'md' => 'px-4 py-2 text-sm',
+        'lg' => 'px-6 py-3 text-base',
+        default => 'px-4 py-2 text-sm',
+    };
 @endphp
-<button type="{{ $type }}" class="px-4 py-2 rounded-lg font-medium text-sm transition {{ $classes }} ">
+<button type="{{ $type }}" {{ $attributes }}
+    class="rounded-lg font-medium transition {{$sizeClasses}} {{ $classes }} ">
     {{ $slot }}
 </button>
