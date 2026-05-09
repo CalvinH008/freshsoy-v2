c83a7000b7e53649feb43fe930f0a7a37fa2abb4
CalvinH008 <tololcapung@gmail.com>
2026-05-09 13:58:08 +0700

    refactor(components): add Tailwind styling to alert component

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,4 +1,13 @@
 @props(['type' => 'success', 'message' => ''])
-<div class="alert alert-{{ $type }}">
+@php
+    $classes = match ($type) {
+        'success' => 'bg-green-50 border border-green-200 text-green-700',
+        'error' => 'bg-red-50 border border-red-200 text-red-700',
+        'warning' => 'bg-yellow-50 border border-yellow-200 text-yellow-700',
+        default => 'bg-gray-50 border border-gray-200 text-gray-700',
+    };
+@endphp
+
+<div class="px-4 py-3 rounded-lg {{ $classes }}">
     {{ $message }}
 </div>
