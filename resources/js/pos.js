3a0089b0b6718db942c96a9f999c529c2e4f3721
CalvinH008 <tololcapung@gmail.com>
2026-05-09 22:56:08 +0700

    feat(pos): add posSystem Alpine function with cart management

--- resources/js/pos.js
+++ resources/js/pos.js
@@ -1,15 +1,69 @@
-export function productForm(initialVariants = []){
+export function productForm(initialVariants = []) {
     return {
         variants: initialVariants,
-        addVariant(){
+        addVariant() {
             this.variants.push({
-                size:'', 
-                price:''
-            })
+                size: "",
+                price: "",
+            });
         },
 
-        removeVariant(index){
-            this.variants.splice(index, 1)
+        removeVariant(index) {
+            this.variants.splice(index, 1);
         },
-    }
+    };
+}
+
+export function posSystem() {
+    return {
+        products: [],
+        cart: [],
+        search: "",
+        outletId: null,
+
+        init() {
+            this.getProduct();
+        },
+
+        async getProduct() {
+            try {
+                const result = await axios.get(
+                    `api/products?outlet_id=${this.outletId}`,
+                );
+                this.products = result.data.data;
+                console.log("data dari backend", this.products);
+            } catch (error) {
+                console.log("error", error);
+            }
+        },
+
+        addToCart(variant, productName) {
+            const existing = this.cart.find(
+                (item) => item.variantId === variant.id,
+            );
+
+            if (existing) {
+                // increment quantity
+                existing.quantity++;
+            } else {
+                // push item baru
+                this.cart.push({
+                    variantId: variant.id,
+                    name: productName,
+                    size: variant.size,
+                    price: variant.price,
+                    stock: variant.stock,
+                    quantity: 1,
+                });
+            }
+        },
+
+        removeFromCart(variantId){
+            this.cart = this.cart.filter(item => item.variantId !== variantId)
+        },
+
+        get total(){
+            return this.cart.reduce((sum, item) => sum + (item.price * item.quantity), 0)
+        }
+    };
 }
